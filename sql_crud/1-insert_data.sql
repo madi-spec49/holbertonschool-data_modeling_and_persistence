cb17441413a6a37bf7e5ff11f20c5bea72dd9093
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-02 12:49:16 +0200

    insert values

--- sql_crud/1-insert_data.sql
+++ sql_crud/1-insert_data.sql
@@ -6,4 +6,4 @@ VALUES
     ('1984', 'George Orwell', 'Dystopian', 8.99, 12, 1949),
     ('Clean Code', 'Robert C.Martin', 'Tech', 32.50, 7, 2008),
     ('The Pragmatic Programmer', 'Andrew Hunt', 'Tech', 28.75, 4, 1999),
-    ('Dune',  'Frank Herbert' , 'Sci-Fi', 9.50, 9, 1965);
+    ('Dune', 'Frank Herbert' , 'Sci-Fi', 9.50, 9, 1965);
