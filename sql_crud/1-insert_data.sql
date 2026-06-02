50b0a6a54cb8b2539754fa2f09b62a032fb7e573
madi-spec49 <madi.anli.madi@gmail.com>
2026-06-02 12:55:10 +0200

    insert values

--- sql_crud/1-insert_data.sql
+++ sql_crud/1-insert_data.sql
@@ -1,7 +1,7 @@
 INSERT INTO books
     (title, author, genre, price, stock, published_year) 
     VALUES
-    ('The Hobbit', 'J.R.R Tolkien', 'Fantasy', 10.99, 5, 1937),
+    ('The Hobbit', 'J.R.R. Tolkien', 'Fantasy', 10.99, 5, 1937),
     ('1984', 'George Orwell', 'Dystopian', 8.99, 12, 1949),
     ('Clean Code', 'Robert C.Martin', 'Tech', 32.50, 7, 2008),
     ('The Pragmatic Programmer', 'Andrew Hunt', 'Tech', 28.75, 4, 1999),
